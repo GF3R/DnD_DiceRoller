ce9a2daac825691845a502e70723fce86427d504
Gabriel Frischknecht <gabriel.frischknecht@isolutions.ch>
2018-09-26 17:46:32 +0200

    added first version¨

---
+++ DiceRoller/dice.js
@@ -0,0 +1,22 @@
+function Dice() {
+  this.value = 0;
+  this.finalValue = 0;
+  this.roll = function (numberD) {
+    this.finalValue = Math.floor(Math.random() * numberD) + 1;
+    console.log("rolled dice: " + this.finalValue);
+    this.value = 0;
+  }
+
+  this.draw = function () {
+    if (this.value != this.finalValue) {
+      this.value = Math.floor(Math.random() * 20) + 1;
+    }
+    text(this.value, width / 2, height / 2);
+  }
+
+  this.animate = function (finalNum) {
+    this.finalValue = finalNum;
+    this.value = 0;
+  }
+
+}
---
+++ DiceRoller/diceRoller.js
@@ -0,0 +1,41 @@
+var dice;
+input = "";
+
+function setup() {
+  createCanvas(800, 800);
+  dice = new Dice();
+  textAlign(CENTER, CENTER);
+  textSize(width / 3);
+  frameRate(7);
+}
+
+function draw() {
+  background(200);
+  dice.draw();
+
+
+}
+
+function keyPressed() {
+
+  if (keyCode === 37) {
+    fill(0);
+    value = dice.roll(20);
+  }
+  if (keyCode === 32) {
+    dice.animate(input);
+    input = "";
+  } else {
+    if (!isNaN(String.fromCharCode(keyCode))) {
+      input += String.fromCharCode(keyCode);
+      console.log(input);
+      
+    }
+  }
+
+  if (keyCode === 8){
+    input = "";
+    console.log(input);
+  }
+
+}
